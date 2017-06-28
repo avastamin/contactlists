a3cc25a826dda20a2d359030ed8c7be2f733a46f
Ruhul Amin <r_amin105@yahoo.com>
2017-06-29 00:11:24 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+
+
+
+class App extends Component {
+
+  state = {
+     contacts :[
+      {
+        "id": "ruhul",
+        "name": "Ruhul Amin",
+        "email": "r_amin105@yahoo.com",
+        "avatarURL": "http://localhost:5001/ruhul.jpg"
+      },
+      {
+        "id": "ayesha",
+        "name": "Ayesha Amin",
+        "email": "avastamin@gmail.com",
+        "avatarURL": "http://localhost:5001/ayesha.jpg"
+      },
+      {
+        "id": "rodela",
+        "name": "Rumaisa Amin",
+        "email": "avastamin@gmail.com",
+        "avatarURL": "http://localhost:5001/rodela.jpg"
+      }
+    ]
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+       contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
+  render() {
+    return (
+      <div>
+        <ListContacts
+           contacts= {this.state.contacts}
+           onDeleteContacts={this.removeContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
